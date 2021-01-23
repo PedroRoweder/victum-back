4e1585ce64478d31a8713ffcb9490fc2518a16b7
Pedro Roweder <pedrohroweder@gmail.com>
2021-01-23 11:36:30 -0300

    [FIX] Fixes operation schema

--- src/schema/operation.js
+++ src/schema/operation.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
 
 const operationSchema = new mongoose.Schema({
-  operationTitle: String,
-  status: String, //DOING, DONE, TODO.
-  steps: Array,
+  SKU: String,
+  desc: String,
+  operationList: [
+    {
+      operationTitle: String,
+      status: String, //DOING, DONE, TODO.
+      steps: Array,
+    },
+  ],
 });
 
 module.exports = operationSchema;
